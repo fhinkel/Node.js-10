da1e3367ed73d50b75a05e8afcdf8060c6eda573
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2018-12-02 17:12:53 -0500

    Initial import

--- nodejs/readFile/index.js
+++ nodejs/readFile/index.js
@@ -1,11 +1,8 @@
-// const fs = require('fs');
+// fs (file system)
+let fs = require('fs');
 
-// fs.readFile('./file.txt', (err, data) => {
-//     console.log(data.toString());
-// })
+let main = async() => {
 
-const fs = require('fs').promises;
+};
 
-
-let data = await fs.readFile('./file.txt');
-console.log(data.toString());
+main();
